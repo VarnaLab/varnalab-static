a255c66f99a5669bdb2056fc2540f1e109188193
simov <simeonvelichkov@gmail.com>
2018-01-31 07:43:39 +0200

    Extract the transformations into separate module

--- bin/http.js
+++ bin/http.js
@@ -1,8 +1,7 @@
 
 var request = require('@request/client')
 var purest = require('purest')({request, promise: Promise})
-var moment = require('moment')
-moment.locale('bg')
+var transform = require('./transform')()
 
 
 module.exports = (config) => {
@@ -15,72 +14,19 @@ module.exports = (config) => {
     }
   })
 
-  var t = {
-    start: (event) => (
-      event.time1 = moment(event.start_time).format('LL'),
-      event.time2 = moment(event.start_time).format('LLLL').split(',')[0],
-      event.time3 = moment(event.start_time).format('LT')
-    )
-    ,
-    links: (event) =>
-      event.description = (event.description || '')
-        .replace(/\n/gi, '<br>')
-        .replace(
-          /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi,
-          (url) => `<a href="${url}">${url}</a>`
-        )
-    ,
-    live: (event) => event.live =
-      new Date(event.start_time).getTime()
-        <= new Date().getTime()
-      &&
-      new Date(event.end_time || event.start_time).getTime()
-        >= new Date().getTime()
-    ,
-    cover: (event) =>
-      event.cover_desktop = event.cover_desktop || 'https://i.imgur.com/VC5g7gp.jpg'
-    ,
-    created: (article) => ((date = new Date(article.date)) =>
-      article.url = [
-        'blogs',
-        date.getFullYear(),
-        date.getMonth() + 1,
-        date.getDate(),
-        article.slug
-      ].join('/')
-    )()
-    ,
-    url: (article) => ((date = new Date(article.date)) => (
-      article.time1 = moment(date).format('LL'),
-      article.time2 = moment(date).format('LLLL').split(',')[0],
-      article.time3 = moment(date).format('LT')
-    ))()
-    ,
-  }
-
   var upcoming = () =>
     varnalab
       .get('events/upcoming')
       .request()
-      .then(([res, body]) => body
-        .map((event) => (
-          t.start(event), t.links(event),  t.cover(event), t.live(event),
-          event
-        ))
-      )
+      .then(([res, body]) => transform.events(body))
 
   var events = () =>
     varnalab
       .get('events')
       .qs({limit: 100000})
       .request()
-      .then(([res, body]) => body
-        .filter((event) => new Date(event.start_time) < new Date())
-        .map((event) => (
-          t.start(event), t.links(event), t.cover(event),
-          event
-        ))
-      )
+      .then(([res, body]) => transform.events(
+        body.filter((event) => new Date(event.start_time) < new Date())))
 
   var members = () =>
     varnalab
@@ -92,48 +38,13 @@ module.exports = (config) => {
     varnalab
       .get('articles')
       .request()
-      .then(([res, body]) => body
-        .map((article) => (t.created(article), t.url(article), article))
-      )
+      .then(([res, body]) => transform.articles(body))
 
   var cashbox = () =>
     varnalab
       .get('finance/invbg/cashbox')
       .request()
-      .then(([res, body]) => {
-        var entries = body
-        // filter out removed items
-        .filter((item) => /^\d+$/.test(item['Движение номер']))
-        .map((item) => ({
-          id: parseInt(item['Движение номер']),
-          date: item['Дата'],
-          type:
-            item['Тип'] === 'Приход' ? 'income' :
-            item['Тип'] === 'Разход' ? 'expense' : '',
-          amount: parseFloat(item['Стойност']),
-          reason: item['Основание'],
-          category: item['Категория'],
-          notes: item['Забележки'],
-          admin: item['Контрагент'],
-        }))
-        .sort((a, b) => b.id - a.id)
-
-        var income = entries
-          .filter((entry) => entry.type === 'income')
-          .reduce((sum, entry) => sum += entry.amount, 0.0)
-
-        var expense = entries
-          .filter((entry) => entry.type === 'expense')
-          .reduce((sum, entry) => sum += entry.amount, 0.0)
-
-        return {
-          entries,
-          total: {
-            income: income.toFixed(2),
-            expense: expense.toFixed(2),
-            current: (income - -expense).toFixed(2)}
-        }
-      })
+      .then(([res, body]) => transform.cashbox(body))
 
   return {upcoming, events, members, articles, cashbox}
 }
